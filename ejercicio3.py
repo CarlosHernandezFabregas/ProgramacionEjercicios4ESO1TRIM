690cfe0f1af6daf99de6301f955a761791ed02e6
CarlosHernandezFabregas <144818373+CarlosHernandezFabregas@users.noreply.github.com>
2023-09-13 17:17:49 +0200

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,13 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #3. Programa que pida dos números enteros y realice la suma correspondiente
+>>> Var1=3
+>>> Var2=4
+>>> sum=Var1+Var2
+>>> print(sum)
+7
+>>> Var3=6
+>>> Var4=6
+>>> sum=Var3+Var4
+>>> print(sum)
+12
---
+++ ejercicio5.py
@@ -0,0 +1,34 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #5. Programa que pida cinco palabras y muestre una frase con las cinco. Modifica el código para que entre palabra y palabra haya una coma.
+>>> print("Introduce el primer dia de la semana")
+Introduce el primer dia de la semana
+>>> Var1="Lunes"
+>>> print("Introduce el segundo día de la semana")
+Introduce el segundo día de la semana
+>>> Var2="Martes"
+>>> print("Introduce el tercer día de la semana")
+Introduce el tercer día de la semana
+>>> Var3="Miércoles"
+>>> print("Introduce el cuarto día de la semana")
+Introduce el cuarto día de la semana
+>>> Var4="Jueves"
+>>> print("Introduce el quinto día de la semana")
+Introduce el quinto día de la semana
+>>> Var5="Viernes"
+>>> print("Respuesta"Var1,Var2,Var3,Var4,Var5)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print("Respuesta")
+Respuesta
+>>> print(Var1,Var2,Var3,Var4,Var5)
+Lunes Martes Miércoles Jueves Viernes
+>>> Var1="Lunes,"
+>>> Var2="Martes,"
+>>> Var3="Miércoles,"
+>>> Var4="Jueves,"
+>>> Var5="Viernes,"
+>>> print("Resultado final")
+Resultado final
+>>> Var5="Viernes"
+>>> print(Var1,Var2,Var3,Var4,Var5)
+Lunes, Martes, Miércoles, Jueves, Viernes
