85f165f8d240936f7e5eb417291b90ed733fb034
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-09-28 09:08:11 +0200

    Delete ejercicio12.py

--- ejercicio12.py
+++
@@ -1,6 +0,0 @@
-#12. Realiza un programa que, introduciendo en los valores de lado, base menor, base mayor y altura de un trapecio isósceles, nos devuelva por pantalla en el área y el perímetro.
-var1=int(input("Introduce el lado del trapecio"))
-var2=int(input("Introduce la base menor del trapecio"))
-var3=int(input("Introduce la base mayor del trapecio"))
-var4=int(input("Introduce la altura del trapecio"))
-print("El perímetro del trapecio isósceles es",var1+var1+var2+var3, "El área del trapecio isósceles es",(var3+var2)*var4/2)
