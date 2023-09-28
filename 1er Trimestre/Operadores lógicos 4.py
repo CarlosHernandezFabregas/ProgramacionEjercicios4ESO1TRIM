5d6ced3e4444654e507606129446c1d7720ce1b5
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-09-28 09:52:55 +0200

    Create Operadores lógicos 4.py

---
+++ 1er Trimestre/Operadores lógicos 4.py
@@ -0,0 +1,9 @@
+#4)
+var1=int(input("introduce un número"))
+if var1<0 or var1>10:print("Lo siento, no se puede ejecutar el programa")
+else:
+     if var1==5:print("Has aprobado con un 5")
+     if var1>5:print("Has aprobado")
+     if var1<5:print("Has suspendido")
+     if var1!=5:print("El número es diferente a 5")
+     
