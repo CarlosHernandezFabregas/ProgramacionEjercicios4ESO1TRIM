c050f5e9be577969bbdaafed142dfd131abfa639
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-09-28 09:35:54 +0200

    Create Operadores lógicos 3.py

---
+++ 1er Trimestre/Operadores lógicos 3.py
@@ -0,0 +1,8 @@
+#3)
+var1=int(input("Introduce un número"))
+if var1>10 or var1<0:print("Lo siento, no se puede ejecutar el programa")
+else:   
+     if var1==5: print("El número es igual a 5")
+     if var1!=5:print("El número es diferente a 5")
+     if var1<5:print("El número es menor que 5")
+     else:print("El número es mayor que 5")
