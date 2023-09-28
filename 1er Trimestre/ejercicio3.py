f222f523db2e174de45559cbed1cdc7add9592aa
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-09-28 09:07:19 +0200

    Create ejercicio3.py

---
+++ 1er Trimestre/ejercicio3.py
@@ -0,0 +1,7 @@
+#3. Programa que pida dos números enteros y realice la suma correspondiente
+var1=int(input("Introduce un número")
+var2=int(input("Introduce otro número"))            
+print(var1+var2)             
+var3=int(input("Introduce un número")
+var4=int(input("Introduce otro número"))
+print(var3+var4)
