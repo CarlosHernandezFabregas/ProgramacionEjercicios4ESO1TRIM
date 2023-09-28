208fa428bdf081db4432561fa5a00b5563567989
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-09-28 09:52:52 +0200

    Create Ejercicio aprobado suspendido python.py

---
+++ 1er Trimestre/Ejercicio aprobado suspendido python.py
@@ -0,0 +1,5 @@
+#1) Realizar un programa que el usuario introduzca un valor por teclado. Si ese valor es menor que 5 aparecerá un mensaje indicando "suspendido", si el valor es superior o igual a 5, aprobado
+var1=float(input("Introduce un valor del 0 al 10"))
+if var1<5:print("Suspendido")
+if var1>=5:print("Aprobado")
+    
