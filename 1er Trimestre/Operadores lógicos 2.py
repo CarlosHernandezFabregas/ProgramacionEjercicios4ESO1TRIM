7198c3668827a37be071e08b753b1b6661bbc446
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-09-28 09:35:50 +0200

    Create Operadores lógicos 2.py

---
+++ 1er Trimestre/Operadores lógicos 2.py
@@ -0,0 +1,6 @@
+#2) Realizar un programa que el usuario introduzca un número por teclado utilizando los operadores lógicos ==,!=,>,< determine la comparación con el número 5 indicando por pantalla si el valor introducido es igual, diferente, menor o mayor
+var1=int(input("Introduce un número"))
+if var1==5:print("El número es igual que 5")
+if var1!=5:print("El número es diferente que 5")
+if var1<5:print("El número es menor que 5")
+else:print("El número es mayor que 5")
