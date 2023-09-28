d2c934345354818595286f69d23dcd244c8f64c2
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-09-28 09:07:29 +0200

    Create ejercicio6.py

---
+++ 1er Trimestre/ejercicio6.py
@@ -0,0 +1,9 @@
+#6. A partir del programa 5. Haz que se muestre por pantalla también la frase en el orden inverso en que se han introducido las palabras.
+var1=str (input("Introduce el primer dia de la semana con una coma detrás"))
+var2=str (input("Introduce el segundo dia de la semana con una coma detrás"))
+var3=str (input("Introduce el primer dia de la semana con una coma detrás"))
+var4=str (input("Introduce el cuarto dia de la semana con una coma detrás"))
+var5=str (input("Introduce el quinto dia de la semana sin coma detrás"))
+print(var1,var2,var3,var4,var5)
+print("Ahora al revés")
+print(var5,",",var4,var3,var2,var1)
