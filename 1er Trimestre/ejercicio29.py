e1e9a8c01099f6f6bb212262b846dc3c69a10d33
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-10-17 10:43:39 +0200

    Create ejercicio29.py

---
+++ 1er Trimestre/ejercicio29.py
@@ -0,0 +1,5 @@
+#29. Busca Internet qué función permite obtener la longitud de un String, realiza un programa 
+#que al introducir una frase devuelva la longitud
+var1=str(input("Introduce una frase cualquiera: "))
+var_long=len(var1)
+print("La longitud de la frase es: ",var_long)
