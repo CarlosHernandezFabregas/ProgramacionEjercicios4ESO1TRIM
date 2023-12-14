82fd1833f52b7b15301c8f67d4866913d81371c6
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-12-14 09:19:20 +0100

    Delete ejercicio37.py

--- 1er Trimestre/ejercicio37.py
+++
@@ -1,6 +0,0 @@
-#37. Programa que pregunte cuantas notas quiero introducir y para cada nota diga si estoy aprobado o suspendido.
-var_n=int(input("Introduce el número de notas"))
-for cont in range(var_n):
-    var_not=float(input("Introduce la nota"))
-    if var_not<=4.5:print("Nota suspendida")
-    else:print("Nota aprobada")
