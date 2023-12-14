2972f6f6a46d7358b74c071a4b7244242aad763e
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-12-14 09:24:05 +0100

    Create ejercicio51.py

---
+++ 1er Trimestre/Ejercicios 50-68/ejercicio51.py
@@ -0,0 +1,7 @@
+#51. A partir del programa anterior, modifica el código para que sea el usuario quién introduzca el 
+#número de veces que desea que repita la frase Buenos días. Con While
+var1=int(input("Introduce el número de veces que quieres repetir el programa"))
+var2=0
+while var2<var1:
+    var2=var2+1
+    print("Buenos días")
