bdfc235df22561fe84cb418db476f9c18e09fc65
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-12-14 09:19:16 +0100

    Delete ejercicio36.py

--- 1er Trimestre/ejercicio36.py
+++
@@ -1,8 +0,0 @@
-#36. Programa que sume los n primeros números naturales. n Lo introduce el usuario
-var_n=int(input("Introduce el numero de numeros naturales que quieres sumar"))
-num=0
-contador=0
-for cont in range(var_n):
-    num=num+1+contador
-    contador=contador+1
-print(num)
