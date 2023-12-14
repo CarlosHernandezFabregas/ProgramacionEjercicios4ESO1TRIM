a7539f40df464d0bf522929f7e7d493ebcf72e41
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-12-14 09:20:53 +0100

    Create ejercicio9.py

---
+++ 1er Trimestre/Ejercicios 1-13/ejercicio9.py
@@ -0,0 +1,3 @@
+#9. Programa que pida los segundos y muestre por pantalla y en la misma frase los minutos y las horas
+var1=int(input("Introduce el número de segundos"))
+print("El número de minutos es", var1/60, "El número de horas es", var1/3600)
