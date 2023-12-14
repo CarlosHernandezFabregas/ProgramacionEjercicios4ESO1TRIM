0fddde76846dc71866788071d785d196cf3f3c92
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-12-14 09:24:35 +0100

    Create ejercicio58.py

---
+++ 1er Trimestre/Ejercicios 50-68/ejercicio58.py
@@ -0,0 +1,16 @@
+#58. Modifica el programa anterior para que tengas 3 intentos. Utiliza while
+import random
+num=random.randint(1,5)
+intentos=0
+while intentos<3:
+    intentos=intentos+1
+    num2=float(input("Introduce un número entero entre el 1 y el 5"))
+    if num2>1 and num2<2 or num2>2 and num2<3 or num2>3 and num2<4 or num2>4 and num2<5:
+        print("El número introducido no corresponde a un número entero")
+    if num2<1 or num2>5:
+        print("A ver, te he dicho entre el 1 y el 5, por favor, sigue las normas")
+    if num2>=1 and num2<=5:
+        if num2==num:
+            print("Has acertado el número")
+            break
+        else:print("No has acertado el número, prueba suerte otra vez")
