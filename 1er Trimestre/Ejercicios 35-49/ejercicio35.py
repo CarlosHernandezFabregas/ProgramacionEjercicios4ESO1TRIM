f453d84861b3102abf3680a012d4c3dbcf45743a
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-12-14 09:21:53 +0100

    Create ejercicio35.py

---
+++ 1er Trimestre/Ejercicios 35-49/ejercicio35.py
@@ -0,0 +1,5 @@
+#35. Programa que al introducir un número por teclado permita mostrar ese número de veces tu 
+#nombre
+var_veces=int(input("Introduce un número de veces para repetir el nombre"))
+var_nombre=(input("Introduce tu nombre"))
+for cont in range(var_veces):print(var_nombre)
