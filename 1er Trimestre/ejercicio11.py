1f48421800452a0489550d275fc2e30cfd849059
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-12-14 09:17:56 +0100

    Delete ejercicio11.py

--- 1er Trimestre/ejercicio11.py
+++
@@ -1,4 +0,0 @@
-#11. Realiza un programa que introduciendo el valor del lado de un cuadrado nos devuelva por pantalla en el área y el perímetro.
-var1=int(input("Introduce el lado de el cuadrado"))
-print("El perímetro del cuadrado es",var1*4,".El área del cuadrado es",var1*var1)
-         
