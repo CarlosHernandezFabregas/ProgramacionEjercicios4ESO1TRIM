af80ab17a772a1f26756f95aa2e6adb5d6a29853
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-12-14 09:24:42 +0100

    Create ejercicio61.py

---
+++ 1er Trimestre/Ejercicios 50-68/ejercicio61.py
@@ -0,0 +1,9 @@
+#61. A partir del código anterior, haz que el programa finalice si el valor de la tabla de multiplicar es
+#superior o igual a 40.
+var1=int(input("Introduce un número"))
+mult=0
+var2=0
+while var2<40:
+    var2=var1*mult
+    mult=mult+1
+    print(var2)
