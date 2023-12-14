e48993e3eead22eab304a30d42a1dcd18d6e30fd
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-12-14 09:19:58 +0100

    Delete ejercicio5.py

--- 1er Trimestre/ejercicio5.py
+++
@@ -1,8 +0,0 @@
-#5. Programa que pida cinco palabras y muestre una frase con las cinco. Modifica el código para que entre palabra y palabra haya una coma.
-var1=str (input("Introduce el primer dia de la semana con una coma detrás"))
-var2=str (input("Introduce el segundo dia de la semana con una coma detrás"))
-var3=str (input("Introduce el primer dia de la semana con una coma detrás"))
-var4=str (input("Introduce el cuarto dia de la semana con una coma detrás"))
-var5=str (input("Introduce el quinto dia de la semana sin coma detrás"))
-print(var1,var2,var3,var4,var5)
-
