00e27b11b12e95558a91def7efdbf65bb282f640
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2023-12-14 09:19:23 +0100

    Delete ejercicio38.py

--- 1er Trimestre/ejercicio38.py
+++
@@ -1,9 +0,0 @@
-#38. A partir del programa anterior, establece los rangos para que el usuario no pueda introducir 
-#notas inferiores a 0 y superiores a 10
-var_n=int(input("Introduce el número de notas"))
-for cont in range(var_n):
-    var_not=float(input("Introduce la nota"))
-    if var_not<0 and var_not>10:print("No te pases de listo, calcula con notas reales, del 0 al 10")
-    if var_not>=0 and var_not<=10:
-        if var_not<=4.9:print("Nota suspendida")
-        else:print("Nota aprobada")
