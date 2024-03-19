9387caa178bc9cb6c374e3edec19c2af3e0d6a7d
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2024-03-19 10:05:27 +0100

    Create P42042.py

---
+++ JutgeProgramming/P42042.py
@@ -0,0 +1,8 @@
+#P42042
+l=input()
+if l.isupper():
+    print("uppercase")
+else:print("lowercase")
+if l.lower()=="a" or l.lower()=="e" or l.lower()=="i" or l.lower()=="u" or l.lower()=="o":
+    print("vowel")
+else:(print("consonant"))
