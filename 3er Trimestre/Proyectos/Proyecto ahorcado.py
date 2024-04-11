3c07228e4b5198f6110453d33abaf8e51e9257f7
CarlosHernandezFabregas <carlos.hernandez@campus.xaloc.org>
2024-04-11 09:40:07 +0200

    Update Proyecto ahorcado.py

--- 3er Trimestre/Proyectos/Proyecto ahorcado.py
+++ 3er Trimestre/Proyectos/Proyecto ahorcado.py
@@ -1,6 +1,8 @@
 #AHORCADO Diseña un programa que simule el juego del Ahorcado teniendo en cuenta las indicaciones descritas en
 #los siguientes bloques.
 import random
+import time
+start=time.perf_counter()
 lista_palabrasecreta=["casa","coche","lavadora","titanic","supermercado","episodio","telenovela","jardinero","amapola","guarderia"]
 lista_partida=[]
 lista_ahorcado=[]
@@ -21,12 +23,13 @@ for cont in palabra:
 for cont in range(len(palabra)):
     lista_partida.append("_")
 print(lista_partida)
-while lista_ahorcado!=lista_fin and lista_partida!=palabracorrecta:
+while lista_ahorcado!=lista_fin and lista_partida!=palabracorrecta: #Primer bucle para establecer que no se ha acabado
     letras=input("Introduce una letra que se introduzca en la palabra:")
+    letras=letras.lower()
     x=1
     fallo=0
     acierto=0
-    for cont in range(len(palabra)):
+    for cont in range(len(palabra)): #Para recorrer el string
         if letras==palabra[cont]:
             x=palabra.count(letras)
             lista_partida.pop(cont)
@@ -35,7 +38,7 @@ while lista_ahorcado!=lista_fin and lista_partida!=palabracorrecta:
             veces=veces+1
             fallo=0
             lista_aciertos.append(letras)
-        if palabra.count(letras)<1:
+        if palabra.count(letras)<1: #Si la letra no está en la palabra, que solo haya 1 error
             fallo=fallo+1
         if x!=acierto and fallo==1:
                 fallo=fallo+1
@@ -46,21 +49,23 @@ while lista_ahorcado!=lista_fin and lista_partida!=palabracorrecta:
                     print(lista_ahorcado)
                     if len(lista_ahorcado)==8:
                          fin=1
-    if lista_ahorcado==lista_fin or lista_partida==palabracorrecta:
+    if lista_ahorcado==lista_fin or lista_partida==palabracorrecta: #Si cualquiera de las 2 listas está completa, que se acabe la partida
         fin=1
     print(lista_partida)
     if fin==1:
+        final=time.perf_counter()-start
         print("El número de aciertos ha sido de",len(lista_aciertos))
         print("El número de errores ha sido de",len(lista_errores))
-        repe=input("¿Quieres hacer una nueva partida? si para si, no para no:")
-        if repe=="si":
+        print("El tiempo que se ha tardado ha sido de",final)
+        repe=input("¿Quieres hacer una nueva partida? si para si, no para no:") #Para hacer una nueva partida
+        if repe=="si": #Se reinician todas las variables
             fin=0
             veces=0
             acierto=0
             fallo=0
             pos=0
             lista_palabrasecreta.pop(num)
-            preg=input("¿Deseas introducir una nueva palabra? si para si, no para no")
+            preg=input("¿Deseas introducir una nueva palabra? si para si, no para no") #Para introducir una nueva palabra
             if preg=="si":
                 lista_palabrasecreta.insert(num,input("Introduce la nueva palabra que quieres que se añada a la lista:"))
             num=random.randint(0,9)
@@ -77,3 +82,4 @@ while lista_ahorcado!=lista_fin and lista_partida!=palabracorrecta:
             for cont in range(len(palabra)):
                 lista_partida.append("_")
             print(lista_partida)
+            start=time.perf_counter()
